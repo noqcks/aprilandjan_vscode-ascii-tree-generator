1bb86942291d06ee56901dafcdc97a3d2e887cb6
noqcks <benji@093b.org>
2024-04-07 01:16:45 +0000

    Upgrade dependencies

--- src/lib/directory.ts
+++ src/lib/directory.ts
@@ -4,7 +4,8 @@ import * as path from 'path';
 import { promisify } from 'util';
 import { IFileStat, IFileTreeItem, IListDirectoryConfig } from './interface';
 
-const glob = promisify(rawGlob);
+import * as rawGlob from 'glob';
+const glob = promisify(rawGlob.glob);
 const readStat = promisify(fs.stat);
 
 /**
